f771b7c5c69029e7f27514323743360845114e7a
sashaDoubov <doubovs@gmail.com>
2018-03-09 10:33:09 -0500

    Cleaning up the motion detector with better time management

--- motion_detector.py
+++ motion_detector.py
@@ -5,7 +5,25 @@ import cv2
 import datetime
 import imutils
 import pudb
+import sys
+import os
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if len(sys.argv) != 2:
+    print("Improper cmd-line args, must pass in path to save to!")
+    sys.exit(0)
+
+homeDir = sys.argv[1]
+
+if not os.path.isdir(homeDir):
+    print("Not a directory path!")
+    sys.exit(0)
+
+# camera init
+logger.info("Initializing Camera...")
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 16
@@ -13,16 +31,42 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # necessary to warm-up
 time.sleep(0.1)
+logger.info("Done initializing")
 
 avg = None
 motionCounter = 0
-lastUploaded = datetime.datetime.now()
+
+lastUploaded = None
+numUploaded = 0
+
+prevMotionTime = datetime.datetime.now()
+now = datetime.datetime.now()
+today6pm = now.replace(hour=18, minute=0, second=0,microsecond=0)
+
+dayString = now.strftime("%b-%d")
+uploadPath = os.path.join(homeDir, dayString)
+
+logger.info("Creating directory for today")
+
+# safely create today's directory
+try:
+    os.makedirs(uploadPath)
+except OSError:
+    if not os.path.isdir(uploadPath):
+        raise
+
+logger.info("Entering camera capturing loop...")
 
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     frame = f.array
     timestamp = datetime.datetime.now()
-    roomOccupied = False 
-    text = "Unoccupied"
+
+    if timestamp > today6pm:
+        logger.info("timestamp exceeds 6pm mark, exiting program!")
+        # stop gathering 
+        sys.exit(0)
+
+    roomOccupied = False
 
     frame = imutils.resize(frame, width = 500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
@@ -52,19 +96,22 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
     
     if roomOccupied:
-        print("occupied!")
-        if (timestamp - lastUploaded).seconds >= 3:
+        if (timestamp - prevMotionTime).seconds >= 3: 
             motionCounter += 1
 
             if motionCounter >= 8:
-                cv2.imshow("Frame", frame)
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord("q"):
-                    break
 
-                lastUploaded = timestamp
+                prevMotionTime = timestamp
                 motionCounter = 0
 
+                if lastUploaded is None or (timestamp - lastUploaded).seconds >= 120: 
+                    filePath = os.path.join(uploadPath, "cap_{}.jpg".format(numUploaded))
+                    logger.info("Uploading image: {}".format(filePath))
+
+                    cv2.imwrite(filePath, f.array)
+                    lastUploaded = timestamp
+                    numUploaded += 1
     else:
         motionCounter = 0
+
     rawCapture.truncate(0)
